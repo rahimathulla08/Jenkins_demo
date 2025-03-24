9a9d802751c232b2d391def20107de886b5a840f
rahimathulla08 <168528448+rahimathulla08@users.noreply.github.com>
2025-03-24 13:08:30 +0530

    Update Cmdline2.java

--- Cmdline2.java
+++ Cmdline2.java
@@ -6,7 +6,7 @@ String s1=args[0];
 String s2=args[1];
 String s3=args[2];
 
-System.out.println("Name"+s1);
+System.out.println("Name:"+s1);
 System.out.println("Roll no:"+s2);
 System.out.println("Branch:"+s3);
 
