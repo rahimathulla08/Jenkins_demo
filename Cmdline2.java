0f6764e9a81f5f44d0303bdd7d1c0a95d06f49b5
rahimathulla08 <168528448+rahimathulla08@users.noreply.github.com>
2025-03-24 12:53:34 +0530

    Add files via upload

---
+++ Cmdline2.java
@@ -0,0 +1,14 @@
+class Cmdline2
+{
+public static void main(String args[])
+{
+String s1=args[0];
+String s2=args[1];
+String s3=args[2];
+
+System.out.println("Name"+s1);
+System.out.println("Roll no:"+s2
+System.out.println("Branch:"+s3);
+
+}
+}
